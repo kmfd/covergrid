50240b39382f7f39ce9b56944a3fef8566696553
kmfd <47769987+kmfd@users.noreply.github.com>
2024-09-19 02:31:48 -0500

    added no-cover.png and grid slider

--- get_covers.php
+++ get_covers.php
@@ -4,13 +4,18 @@ header('Content-Type: application/json');
 $directory = 'covers/';
 
 // Get all files in the directory
-// Exclude . and ..
 $files = array_diff(scandir($directory), array('..', '.'));
 
 // Filetype filter
 $imageFiles = array_filter($files, function($file) {
     return preg_match('/\.(jpg|jpeg|png|gif)$/i', $file);
 });
+
+// Check if there are no images in the covers directory
+if (count($imageFiles) === 0) {
+    // If no images found, include no-cover.png from the root directory
+    $imageFiles[] = '../no-cover.png';
+}
 
 // Return the list of image files as JSON
 echo json_encode(array_values($imageFiles));
